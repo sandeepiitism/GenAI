54fbfd4b6a467408ac42d5eb4dad303b0cfaf54b
Sandeep Chowdhury <49147378+sandeepiitism@users.noreply.github.com>
2024-11-05 08:57:26 +0530

    Add files via upload

---
+++ PDFSummarizer/app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pdfplumber
+from transformers import BartTokenizer, BartForConditionalGeneration
+
+# Title
+st.title("PDF Text Summarizer")
+
+# Upload PDF
+pdf_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+
+if pdf_file:
+    with pdfplumber.open(pdf_file) as pdf:
+        # Extract text from the PDF
+        pdf_text = ""
+        for page in pdf.pages:
+            pdf_text += page.extract_text()
+
+        # Initialize BART model and tokenizer
+        model = BartForConditionalGeneration.from_pretrained('sshleifer/distilbart-cnn-12-6')
+        tokenizer = BartTokenizer.from_pretrained('sshleifer/distilbart-cnn-12-6')
+
+        # Tokenize and generate summary with a maximum of 500 words
+        inputs = tokenizer([pdf_text], truncation=True, return_tensors='pt')
+        summary_ids = model.generate(inputs['input_ids'], num_beams=4, early_stopping=True, max_length=500)  # Set max_length to 500
+        summarized_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
+
+        st.subheader("Summary")
+        st.write(summarized_text)
